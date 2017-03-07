195a2fa7ec129e9a6b940efd6ff62ed9a86f7e39
qq_37122005 <377655324@qq.com>
2017-03-07 15:56:03 +0800

    up

--- zhaoxiao/build/js/pic.js
+++
@@ -1,2 +0,0 @@
-/* pic.js Date:2017-03-07 15:30:47 */
-define("pic.js",[],function(require,a,b){var c={_init:function(){},alertBox:function(){},addEvent:function(a,b,c){a.addEventListener?a.addEventListener(b,c,!1):a.attachEvent("on"+b,c)},first:function(a){return a?a.firstElementChild||a.firstChild:null},last:function(a){return a?a.lastElementChild||a.lastChild:null}};b.exports={init:c.addEvent}});

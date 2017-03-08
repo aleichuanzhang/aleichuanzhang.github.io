4748a766a1031eed09a25fa27079efa8ac92fcbb
qq_37122005 <377655324@qq.com>
2017-03-08 10:06:09 +0800

    aa

--- zhaoxiao/js/filter.js
+++
@@ -1,2 +0,0 @@
-/* filter.js Date:2017-03-08 09:32:36 */
-define("filter.js",["vue.js"],function(require,a,b){require("vue.js");var c={filter:function(){Vue.filter("typeAge",function(a){return a}),Vue.filter("typeSelect",function(a,b){return console.log(""==a),""==a||"undefined"==typeof a?b:a}),Vue.filter("typeSex",function(a){var b={female:"\u5973",male:"\u7537"};return b[a]})}};b.exports={filter:c.filter}});

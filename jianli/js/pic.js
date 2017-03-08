0083991c23b73e162f6d332a17568abd67c70349
qq_37122005 <377655324@qq.com>
2017-03-08 10:07:37 +0800

    jianli

---
+++ jianli/js/pic.js
@@ -0,0 +1,2 @@
+/* pic.js Date:2017-03-08 09:32:34 */
+define("pic.js",[],function(require,a,b){var c={_init:function(){},alertBox:function(){},addEvent:function(a,b,c){a.addEventListener?a.addEventListener(b,c,!1):a.attachEvent("on"+b,c)},first:function(a){return a?a.firstElementChild||a.firstChild:null},last:function(a){return a?a.lastElementChild||a.lastChild:null}};b.exports={init:c.addEvent}});

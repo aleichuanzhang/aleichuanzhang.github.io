f61d0b91ebbd29fe65791c7352ab6d8e213838b4
qq_37122005 <377655324@qq.com>
2017-03-08 09:39:23 +0800

    wancheng

--- zhaoxiao/js/pic.js
+++ zhaoxiao/js/pic.js
@@ -1,2 +1,2 @@
-/* pic.js Date:2017-03-07 21:09:49 */
+/* pic.js Date:2017-03-08 09:32:34 */
 define("js/pic.js",[],function(require,a,b){var c={_init:function(){},alertBox:function(){},addEvent:function(a,b,c){a.addEventListener?a.addEventListener(b,c,!1):a.attachEvent("on"+b,c)},first:function(a){return a?a.firstElementChild||a.firstChild:null},last:function(a){return a?a.lastElementChild||a.lastChild:null}};b.exports={init:c.addEvent}});

4f03d6428738217ae861cfa8e2906757ee46481c
qq_37122005 <377655324@qq.com>
2017-03-17 17:12:33 +0800

    zuixin

--- jianli/js/filter.js
+++ jianli/js/filter.js
@@ -1,2 +1,2 @@
-/* filter.js Date:2017-03-09 10:57:48 */
-define("filter.js",["vue.js"],function(require,a,b){require("vue.js");var c={filter:function(){Vue.filter("typeAge",function(a){return a}),Vue.filter("typeSelect",function(a,b){return console.log(""==a),""==a||"undefined"==typeof a?b:a}),Vue.filter("typeSex",function(a){var b={female:"\u5973",male:"\u7537"};return b[a]})}};b.exports={filter:c.filter}});
+/* filter.js Date:2017-03-17 17:07:12 */
+define("filter.js",["vue.js"],function(require,a,b){require("vue.js");var c={filter:function(){Vue.filter("typeAge",function(a){return a}),Vue.filter("typeSelect",function(a,b){return""==a||"undefined"==typeof a?b:a}),Vue.filter("typeSex",function(a){var b={female:"\u5973",male:"\u7537"};return b[a]})}};b.exports={filter:c.filter}});
